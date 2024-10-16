58986c0306ebd950d2a2f5ede0e84c62eac1ee57
Frodo Baggins <baggins31084@proton.me>
2024-10-16 09:51:45 -0700

    fix compiler errors after merge
    DONT MERGE set log file to /dev/null
    
    DONT MERGE linux pthread bb fix
    
    save work
    
    DONT MERGE dump ir
    
    save more work
    
    fix mistake with ES shader
    
    skip list
    
    add input patch control points dynamic state
    
    random stuff

--- src/shader_recompiler/backend/spirv/spirv_emit_context.h
+++ src/shader_recompiler/backend/spirv/spirv_emit_context.h
@@ -191,6 +191,7 @@ public:
     Id clip_distances{};
     Id cull_distances{};
 
+    Id patch_vertices{};
     Id output_tess_level_outer{};
     Id output_tess_level_inner{};
     Id tess_coord;
@@ -198,6 +199,7 @@ public:
 
     Id workgroup_id{};
     Id local_invocation_id{};
+    Id invocation_id{}; // for instanced geoshaders or output vertices within TCS patch
     Id subgroup_local_invocation_id{};
     Id image_u32{};
 
