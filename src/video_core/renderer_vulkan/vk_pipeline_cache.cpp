5f20c1aed28bf1822f398792f393d9137a32140e
Dmugetsu <168934208+diegolix29@users.noreply.github.com>
2024-11-13 06:06:19 -0600

    Another loading test

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -226,7 +226,7 @@ const ComputePipeline* PipelineCache::GetComputePipeline() {
 }
 
 bool ShouldSkipShader(u64 shader_hash, const char* shader_type) {
-    static std::vector<u64> skip_hashes = {};
+    static std::vector<u64> skip_hashes = {0xc1888bd};
     if (std::ranges::contains(skip_hashes, shader_hash)) {
         LOG_WARNING(Render_Vulkan, "Skipped {} shader hash {:#x}.", shader_type, shader_hash);
         return true;
