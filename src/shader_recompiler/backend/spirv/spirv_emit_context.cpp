f1f05014ccc0a77eeae19f79ca2945ff3566f0fa
Dmugetsu <diegoavilafacebook75@gmail.com>
2024-12-05 02:37:52 -0600

    May Fix AMD

--- src/shader_recompiler/backend/spirv/spirv_emit_context.cpp
+++ src/shader_recompiler/backend/spirv/spirv_emit_context.cpp
@@ -421,14 +421,12 @@ void EmitContext::DefineInputs() {
             input_params[i] = {id, input_f32, F32[1], 4};
         }
 
-        // TODO is it ok to share output locations between patch consts and
-        // per-vertex output attrs?
-        // spirv-val doesn't complain so idk
+        u32 patch_base_location = runtime_info.vs_info.hs_output_cp_stride >> 4;
         for (size_t index = 0; index < 30; ++index) {
             if (!(info.uses_patches & (1U << index))) {
                 continue;
             }
-            const Id id{DefineInput(F32[4], index)};
+            const Id id{DefineInput(F32[4], patch_base_location + index)};
             Decorate(id, spv::Decoration::Patch);
             Name(id, fmt::format("patch_in{}", index));
             patches[index] = id;
@@ -497,14 +495,12 @@ void EmitContext::DefineOutputs() {
             output_params[i] = {id, output_f32, F32[1], 4};
         }
 
-        // TODO is it ok to share output locations between patch consts and
-        // per-vertex output attrs?
-        // spirv-val doesn't complain so idk
+        u32 patch_base_location = runtime_info.hs_info.hs_output_cp_stride >> 4;
         for (size_t index = 0; index < 30; ++index) {
             if (!(info.uses_patches & (1U << index))) {
                 continue;
             }
-            const Id id{DefineOutput(F32[4], index)};
+            const Id id{DefineOutput(F32[4], patch_base_location + index)};
             Decorate(id, spv::Decoration::Patch);
             Name(id, fmt::format("patch_out{}", index));
             patches[index] = id;
