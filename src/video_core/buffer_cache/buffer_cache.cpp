6b0c4e254673e782f3912ceadfd7ea5d538b2d00
Dmugetsu <diegoavilafacebook75@gmail.com>
2024-12-14 21:31:44 -0600

    roamic particles + sound hack fix, and pm4 type0 fix

--- src/video_core/buffer_cache/buffer_cache.cpp
+++ src/video_core/buffer_cache/buffer_cache.cpp
@@ -231,7 +231,7 @@ void BufferCache::InlineData(VAddr address, const void* value, u32 num_bytes, bo
         .srcStageMask = vk::PipelineStageFlagBits2::eTransfer,
         .srcAccessMask = vk::AccessFlagBits2::eTransferWrite,
         .dstStageMask = vk::PipelineStageFlagBits2::eAllCommands,
-        .dstAccessMask = vk::AccessFlagBits2::eMemoryRead,
+        .dstAccessMask = vk::AccessFlagBits2::eMemoryRead | vk::AccessFlagBits2::eMemoryWrite,
         .buffer = buffer->Handle(),
         .offset = buffer->Offset(address),
         .size = num_bytes,
@@ -241,11 +241,88 @@ void BufferCache::InlineData(VAddr address, const void* value, u32 num_bytes, bo
         .bufferMemoryBarrierCount = 1,
         .pBufferMemoryBarriers = &pre_barrier,
     });
-    cmdbuf.updateBuffer(buffer->Handle(), buffer->Offset(address), num_bytes, value);
+    cmdbuf.updateBuffer(buffer->Handle(), buf_barrier.offset, num_bytes, value);
     cmdbuf.pipelineBarrier2(vk::DependencyInfo{
         .dependencyFlags = vk::DependencyFlagBits::eByRegion,
         .bufferMemoryBarrierCount = 1,
-        .pBufferMemoryBarriers = &post_barrier,
+        .pBufferMemoryBarriers = &buf_barrier,
+    });
+}
+void BufferCache::CopyBuffer(VAddr dst, VAddr src, u32 num_bytes, bool is_dst_gds,
+                             bool is_src_gds) {
+    // Check if the destination region is valid or registered.
+    if (!is_dst_gds && !IsRegionRegistered(dst, num_bytes)) {
+        if (is_src_gds || IsRegionRegistered(src, num_bytes)) {
+            LOG_CRITICAL(Render_Vulkan, "Readback is not implemented for unregistered regions");
+            return;
+        }
+        // Perform direct memory copy for unregistered regions.
+        memcpy(reinterpret_cast<void*>(dst), reinterpret_cast<void*>(src), num_bytes);
+        return;
+    }
+
+    // Check if the source region is valid or registered.
+    if (!is_src_gds && !IsRegionRegistered(src, num_bytes)) {
+        // Inline data for unregistered source regions.
+        InlineData(dst, reinterpret_cast<void*>(src), num_bytes, is_dst_gds);
+        return;
+    }
+
+    // Retrieve source and destination buffers.
+    auto& src_buffer = [&]() -> const Buffer& {
+        if (is_src_gds) {
+            return gds_buffer; // Use the GDS buffer for source.
+        }
+        const BufferId buffer_id = FindBuffer(src, num_bytes);
+        return slot_buffers[buffer_id];
+    }();
+
+    auto& dst_buffer = [&]() -> const Buffer& {
+        if (is_dst_gds) {
+            return gds_buffer; // Use the GDS buffer for destination.
+        }
+        const BufferId buffer_id = FindBuffer(dst, num_bytes);
+        return slot_buffers[buffer_id];
+    }();
+
+    // Define Vulkan buffer copy region.
+    vk::BufferCopy region{
+        .srcOffset = src_buffer.Offset(src),
+        .dstOffset = dst_buffer.Offset(dst),
+        .size = num_bytes,
+    };
+    const vk::BufferMemoryBarrier2 buf_barriers[2] = {
+        {
+            .srcStageMask = vk::PipelineStageFlagBits2::eAllCommands,
+            .srcAccessMask = vk::AccessFlagBits2::eMemoryRead | vk::AccessFlagBits2::eMemoryWrite,
+            .dstStageMask = vk::PipelineStageFlagBits2::eAllCommands,
+            .dstAccessMask = vk::AccessFlagBits2::eMemoryRead | vk::AccessFlagBits2::eMemoryWrite,
+            .buffer = dst_buffer.Handle(),
+            .offset = dst_buffer.Offset(dst),
+            .size = num_bytes,
+        },
+        {
+            .srcStageMask = vk::PipelineStageFlagBits2::eAllCommands,
+            .srcAccessMask = vk::AccessFlagBits2::eMemoryRead | vk::AccessFlagBits2::eMemoryWrite,
+            .dstStageMask = vk::PipelineStageFlagBits2::eAllCommands,
+            .dstAccessMask = vk::AccessFlagBits2::eMemoryRead | vk::AccessFlagBits2::eMemoryWrite,
+            .buffer = src_buffer.Handle(),
+            .offset = src_buffer.Offset(src),
+            .size = num_bytes,
+        },
+    };
+    scheduler.EndRendering();
+    const auto cmdbuf = scheduler.CommandBuffer();
+    cmdbuf.pipelineBarrier2(vk::DependencyInfo{
+        .dependencyFlags = vk::DependencyFlagBits::eByRegion,
+        .bufferMemoryBarrierCount = 2,
+        .pBufferMemoryBarriers = buf_barriers,
+    });
+    cmdbuf.copyBuffer(src_buffer.Handle(), dst_buffer.Handle(), region);
+    cmdbuf.pipelineBarrier2(vk::DependencyInfo{
+        .dependencyFlags = vk::DependencyFlagBits::eByRegion,
+        .bufferMemoryBarrierCount = 2,
+        .pBufferMemoryBarriers = buf_barriers,
     });
 }
 
