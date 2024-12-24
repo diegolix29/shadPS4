817ae1ebd53a72a665292118a76be19b3da9cc05
Dmugetsu <diegoavilafacebook75@gmail.com>
2024-12-24 09:47:41 -0600

    clang and new partbb synced with main shad

--- src/video_core/amdgpu/liverpool.cpp
+++ src/video_core/amdgpu/liverpool.cpp
@@ -25,8 +25,8 @@ static const char* ccb_task_name{"CCB_TASK"};
 static_assert(Liverpool::NumComputeRings <= MAX_NAMES);
 
 #define NAME_NUM(z, n, name) BOOST_PP_STRINGIZE(name) BOOST_PP_STRINGIZE(n),
-#define NAME_ARRAY(name, num)                                                                      \
-    { BOOST_PP_REPEAT(num, NAME_NUM, name) }
+#define NAME_ARRAY(name, num) {BOOST_PP_REPEAT(num, NAME_NUM, name)}
+
 
 static const char* acb_task_name[] = NAME_ARRAY(ACB_TASK, MAX_NAMES);
 
