66fa06206d3180c3b413c98cd975ff02d689cb96
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-02-24 07:03:30 -0600

    redbacks impl

--- src/video_core/amdgpu/liverpool.cpp
+++ src/video_core/amdgpu/liverpool.cpp
@@ -75,6 +75,8 @@ Liverpool::~Liverpool() {
 void Liverpool::Process(std::stop_token stoken) {
     Common::SetCurrentThreadName("shadPS4:GpuCommandProcessor");
 
+    GPUThreadID = std::this_thread::get_id();
+
     while (!stoken.stop_requested()) {
         {
             std::unique_lock lk{submit_mutex};
