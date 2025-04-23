993195fee9476acad8f55c60dc7ea84b906db41a
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-04-23 11:56:45 -0600

    test3

--- src/core/libraries/np_web_api/np_web_api.cpp
+++ src/core/libraries/np_web_api/np_web_api.cpp
@@ -88,8 +88,16 @@ s32 PS4_SYSV_ABI sceNpWebApiAddMultipartPart() {
     return ORBIS_OK;
 }
 
+static bool g_fakeNpWebApiTimedOut = true;
+
 s32 PS4_SYSV_ABI sceNpWebApiCheckTimeout() {
-    LOG_ERROR(Lib_NpWebApi, "(STUBBED) called");
+    LOG_TRACE(Lib_NpWebApi, "sceNpWebApiCheckTimeout called");
+
+    if (g_fakeNpWebApiTimedOut) {
+        LOG_ERROR(Lib_NpWebApi, "Fake timeout reported");
+        return ORBIS_FAIL; // Or whatever error code PS4 returns on timeout
+    }
+
     return ORBIS_OK;
 }
 
