2ccd9acb487755ee8fce895df69d3d6bf50e1431
georgemoralis <giorgosmrls@gmail.com>
2025-05-17 17:33:24 +0300

    New Crowdin updates (#2946)
    * New translations en_us.ts (Korean)
    
    * New translations en_us.ts (Romanian)
    
    * New translations en_us.ts (French)
    
    * New translations en_us.ts (Spanish)
    
    * New translations en_us.ts (Arabic)
    
    * New translations en_us.ts (Danish)
    
    * New translations en_us.ts (German)
    
    * New translations en_us.ts (Greek)
    
    * New translations en_us.ts (Finnish)
    
    * New translations en_us.ts (Hungarian)
    
    * New translations en_us.ts (Italian)
    
    * New translations en_us.ts (Japanese)
    
    * New translations en_us.ts (Lithuanian)
    
    * New translations en_us.ts (Dutch)
    
    * New translations en_us.ts (Polish)
    
    * New translations en_us.ts (Portuguese)
    
    * New translations en_us.ts (Russian)
    
    * New translations en_us.ts (Slovenian)
    
    * New translations en_us.ts (Albanian)
    
    * New translations en_us.ts (Swedish)
    
    * New translations en_us.ts (Turkish)
    
    * New translations en_us.ts (Ukrainian)
    
    * New translations en_us.ts (Chinese Simplified)
    
    * New translations en_us.ts (Chinese Traditional)
    
    * New translations en_us.ts (Vietnamese)
    
    * New translations en_us.ts (Portuguese, Brazilian)
    
    * New translations en_us.ts (Indonesian)
    
    * New translations en_us.ts (Persian)
    
    * New translations en_us.ts (Norwegian Bokmal)
    
    * New translations en_us.ts (Chinese Simplified)
    
    * New translations en_us.ts (Italian)

--- src/qt_gui/translations/de_DE.ts
+++ src/qt_gui/translations/de_DE.ts
@@ -1383,10 +1383,6 @@
       <translation>Spieleliste</translation>
     </message>
     <message>
-      <source> * Unsupported Vulkan Version</source>
-      <translation> * Nicht unterstützte Vulkan-Version</translation>
-    </message>
-    <message>
       <source>Download Cheats For All Installed Games</source>
       <translation>Cheats für alle installierten Spiele herunterladen</translation>
     </message>
@@ -2227,6 +2223,10 @@ Sie können benutzerdefinierte Bilder zu den Trophäen hinzufügen und einen Ton
 Fügen Sie die Dateien dem Ordner custom_trophy mit folgenden Namen hinzu:\n
 trophy.wav ODER trophy.mp3, bronze.png, gold.png, platinum.png, silver.png\n
 Hinweis: Der Sound funktioniert nur in Qt-Versionen.</translation>
+    </message>
+    <message>
+      <source> * Unsupported Vulkan Version</source>
+      <translation type="unfinished"> * Unsupported Vulkan Version</translation>
     </message>
   </context>
   <context>
