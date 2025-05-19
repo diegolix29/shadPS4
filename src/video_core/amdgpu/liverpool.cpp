63d978165b3710cbf5ff3a564373db5eeb0e9175
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-05-19 16:22:42 -0600

    particles test 2 and correct span return

--- src/video_core/amdgpu/liverpool.cpp
+++ src/video_core/amdgpu/liverpool.cpp
@@ -631,6 +631,11 @@ Liverpool::Task Liverpool::ProcessGraphics(std::span<const u32> dcb, std::span<c
                 if (dma_data->dst_addr_lo == 0x3022C || !rasterizer) {
                     break;
                 }
+
+                const u32 num_bytes = dma_data->NumBytes();
+                auto dst_addr = dma_data->DstAddress<VAddr>();
+                auto src_addr = dma_data->SrcAddress<VAddr>();
+
                 auto isMemorySource = [](DmaDataSrc src) {
                     return src == DmaDataSrc::Memory || src == DmaDataSrc::MemoryUsingL2;
                 };
@@ -638,37 +643,45 @@ Liverpool::Task Liverpool::ProcessGraphics(std::span<const u32> dcb, std::span<c
                 if (dma_data->src_sel == DmaDataSrc::Data && dma_data->dst_sel == DmaDataDst::Gds) {
                     rasterizer->InlineData(dma_data->dst_addr_lo, &dma_data->data, sizeof(u32),
                                            true);
+
                 } else if (isMemorySource(dma_data->src_sel) &&
                            dma_data->dst_sel == DmaDataDst::Gds) {
-                    rasterizer->CopyBuffer(dma_data->dst_addr_lo, dma_data->SrcAddress<VAddr>(),
-                                           dma_data->NumBytes(), true, false);
+                    rasterizer->CopyBuffer(dma_data->dst_addr_lo, src_addr, num_bytes, true, false);
+
                 } else if (dma_data->src_sel == DmaDataSrc::Data &&
                            (dma_data->dst_sel == DmaDataDst::Memory ||
                             dma_data->dst_sel == DmaDataDst::MemoryUsingL2)) {
-                    rasterizer->InlineData(dma_data->DstAddress<VAddr>(), &dma_data->data,
-                                           sizeof(u32), false);
+                    rasterizer->InlineData(dst_addr, &dma_data->data, sizeof(u32), false);
+
                 } else if (dma_data->src_sel == DmaDataSrc::Gds &&
-                           dma_data->dst_sel == DmaDataDst::Memory) {
-                    rasterizer->CopyBuffer(dma_data->DstAddress<VAddr>(), dma_data->src_addr_lo,
-                                           dma_data->NumBytes(), false, true);
-                    // LOG_WARNING(Render_Vulkan, "GDS memory read");
+                           (dma_data->dst_sel == DmaDataDst::Memory ||
+                            dma_data->dst_sel == DmaDataDst::MemoryUsingL2)) {
+                    rasterizer->CopyBuffer(dst_addr, dma_data->src_addr_lo, num_bytes, false, true);
+
                 } else if (isMemorySource(dma_data->src_sel) &&
                            (dma_data->dst_sel == DmaDataDst::Memory ||
                             dma_data->dst_sel == DmaDataDst::MemoryUsingL2)) {
-                    if (dma_data->NumBytes() > 2) {
-                        rasterizer->CopyBuffer(dma_data->DstAddress<VAddr>(),
-                                               dma_data->SrcAddress<VAddr>(),
-                                               dma_data->NumBytes() - 2, false, false);
+
+                    if (num_bytes == 2 || num_bytes == 4) {
+                        LOG_WARNING(Render,
+                                    "Padding small DMA transfer of {} bytes to avoid slowdown",
+                                    num_bytes);
+                        const u32 padded_bytes = 2;
+                        rasterizer->CopyBuffer(dst_addr, src_addr, padded_bytes, false, false);
+                    } else if (num_bytes > 0) {
+                        rasterizer->CopyBuffer(dst_addr, src_addr, num_bytes, false, false);
                     } else {
-                        UNREACHABLE_MSG("Invalid NumBytes for memory copy: {}",
-                                        dma_data->NumBytes());
+                        LOG_WARNING(Render, "Ignoring 0-byte DMA transfer.");
                     }
+
                 } else {
-                    UNREACHABLE_MSG("WriteData src_sel = {}, dst_sel = {}",
+                    UNREACHABLE_MSG("Unhandled DMA path: src_sel={} dst_sel={}",
                                     u32(dma_data->src_sel.Value()), u32(dma_data->dst_sel.Value()));
                 }
+
                 break;
             }
+
             case PM4ItOpcode::WriteData: {
                 const auto* write_data = reinterpret_cast<const PM4CmdWriteData*>(header);
                 ASSERT(write_data->dst_sel.Value() == 2 || write_data->dst_sel.Value() == 5);
@@ -837,40 +850,54 @@ Liverpool::Task Liverpool::ProcessCompute(const u32* acb, u32 acb_dwords, u32 vq
             if (dma_data->dst_addr_lo == 0x3022C || !rasterizer) {
                 break;
             }
+
+            const u32 num_bytes = dma_data->NumBytes();
+            auto dst_addr = dma_data->DstAddress<VAddr>();
+            auto src_addr = dma_data->SrcAddress<VAddr>();
+
             auto isMemorySource = [](DmaDataSrc src) {
                 return src == DmaDataSrc::Memory || src == DmaDataSrc::MemoryUsingL2;
             };
+
             if (dma_data->src_sel == DmaDataSrc::Data && dma_data->dst_sel == DmaDataDst::Gds) {
                 rasterizer->InlineData(dma_data->dst_addr_lo, &dma_data->data, sizeof(u32), true);
+
             } else if (isMemorySource(dma_data->src_sel) && dma_data->dst_sel == DmaDataDst::Gds) {
-                rasterizer->CopyBuffer(dma_data->dst_addr_lo, dma_data->SrcAddress<VAddr>(),
-                                       dma_data->NumBytes(), true, false);
+                rasterizer->CopyBuffer(dma_data->dst_addr_lo, src_addr, num_bytes, true, false);
+
             } else if (dma_data->src_sel == DmaDataSrc::Data &&
                        (dma_data->dst_sel == DmaDataDst::Memory ||
                         dma_data->dst_sel == DmaDataDst::MemoryUsingL2)) {
-                rasterizer->InlineData(dma_data->DstAddress<VAddr>(), &dma_data->data, sizeof(u32),
-                                       false);
+                rasterizer->InlineData(dst_addr, &dma_data->data, sizeof(u32), false);
+
             } else if (dma_data->src_sel == DmaDataSrc::Gds &&
-                       dma_data->dst_sel == DmaDataDst::Memory) {
-                rasterizer->CopyBuffer(dma_data->DstAddress<VAddr>(), dma_data->src_addr_lo,
-                                       dma_data->NumBytes(), false, true);
-                // LOG_WARNING(Render_Vulkan, "GDS memory read");
+                       (dma_data->dst_sel == DmaDataDst::Memory ||
+                        dma_data->dst_sel == DmaDataDst::MemoryUsingL2)) {
+                rasterizer->CopyBuffer(dst_addr, dma_data->src_addr_lo, num_bytes, false, true);
+
             } else if (isMemorySource(dma_data->src_sel) &&
                        (dma_data->dst_sel == DmaDataDst::Memory ||
                         dma_data->dst_sel == DmaDataDst::MemoryUsingL2)) {
-                if (dma_data->NumBytes() > 2) {
-                    rasterizer->CopyBuffer(dma_data->DstAddress<VAddr>(),
-                                           dma_data->SrcAddress<VAddr>(), dma_data->NumBytes() - 2,
-                                           false, false);
+
+                if (num_bytes == 2 || num_bytes == 4) {
+                    LOG_WARNING(Render, "Padding small DMA transfer of {} bytes to avoid slowdown",
+                                num_bytes);
+                    const u32 padded_bytes = 2;
+                    rasterizer->CopyBuffer(dst_addr, src_addr, padded_bytes, false, false);
+                } else if (num_bytes > 0) {
+                    rasterizer->CopyBuffer(dst_addr, src_addr, num_bytes, false, false);
                 } else {
-                    UNREACHABLE_MSG("Invalid NumBytes for memory copy: {}", dma_data->NumBytes());
+                    LOG_WARNING(Render, "Ignoring 0-byte DMA transfer.");
                 }
+
             } else {
-                UNREACHABLE_MSG("WriteData src_sel = {}, dst_sel = {}",
+                UNREACHABLE_MSG("Unhandled DMA path: src_sel={} dst_sel={}",
                                 u32(dma_data->src_sel.Value()), u32(dma_data->dst_sel.Value()));
             }
+
             break;
         }
+
         case PM4ItOpcode::AcquireMem: {
             break;
         }
@@ -1002,37 +1029,41 @@ Liverpool::Task Liverpool::ProcessCompute(const u32* acb, u32 acb_dwords, u32 vq
 std::pair<std::span<const u32>, std::span<const u32>> Liverpool::CopyCmdBuffers(
     std::span<const u32> dcb, std::span<const u32> ccb) {
     auto& queue = mapped_queues[GfxQueueId];
+    std::scoped_lock lock{queue.m_access};
 
-    // std::vector resize can invalidate spans for commands in flight
-    ASSERT_MSG(queue.dcb_buffer.capacity() >= queue.dcb_buffer_offset + dcb.size(),
-               "dcb copy buffer out of reserved space");
-    ASSERT_MSG(queue.ccb_buffer.capacity() >= queue.ccb_buffer_offset + ccb.size(),
-               "ccb copy buffer out of reserved space");
+    constexpr size_t kMaxBufferSizeDwords = 256 * 1024;
 
-    queue.dcb_buffer.resize(
-        std::max(queue.dcb_buffer.size(), queue.dcb_buffer_offset + dcb.size()));
-    queue.ccb_buffer.resize(
-        std::max(queue.ccb_buffer.size(), queue.ccb_buffer_offset + ccb.size()));
+    if (queue.dcb_buffer_offset > kMaxBufferSizeDwords) {
+        queue.dcb_buffer.clear();
+        queue.dcb_buffer_offset = 0;
+    }
+    if (queue.ccb_buffer_offset > kMaxBufferSizeDwords) {
+        queue.ccb_buffer.clear();
+        queue.ccb_buffer_offset = 0;
+    }
+    size_t required_dcb_size = queue.dcb_buffer_offset + dcb.size();
+    size_t required_ccb_size = queue.ccb_buffer_offset + ccb.size();
 
-    u32 prev_dcb_buffer_offset = queue.dcb_buffer_offset;
-    u32 prev_ccb_buffer_offset = queue.ccb_buffer_offset;
+    queue.dcb_buffer.resize(std::max(queue.dcb_buffer.size(), required_dcb_size));
+    queue.ccb_buffer.resize(std::max(queue.ccb_buffer.size(), required_ccb_size));
+
+    u32 prev_dcb_offset = queue.dcb_buffer_offset;
+    u32 prev_ccb_offset = queue.ccb_buffer_offset;
     if (!dcb.empty()) {
-        std::memcpy(queue.dcb_buffer.data() + queue.dcb_buffer_offset, dcb.data(),
-                    dcb.size_bytes());
+        std::memcpy(queue.dcb_buffer.data() + prev_dcb_offset, dcb.data(), dcb.size_bytes());
         queue.dcb_buffer_offset += dcb.size();
-        dcb = std::span<const u32>{queue.dcb_buffer.begin() + prev_dcb_buffer_offset,
-                                   queue.dcb_buffer.begin() + queue.dcb_buffer_offset};
+        dcb = {queue.dcb_buffer.begin() + prev_dcb_offset,
+               queue.dcb_buffer.begin() + queue.dcb_buffer_offset};
     }
 
     if (!ccb.empty()) {
-        std::memcpy(queue.ccb_buffer.data() + queue.ccb_buffer_offset, ccb.data(),
-                    ccb.size_bytes());
+        std::memcpy(queue.ccb_buffer.data() + prev_ccb_offset, ccb.data(), ccb.size_bytes());
         queue.ccb_buffer_offset += ccb.size();
-        ccb = std::span<const u32>{queue.ccb_buffer.begin() + prev_ccb_buffer_offset,
-                                   queue.ccb_buffer.begin() + queue.ccb_buffer_offset};
+        ccb = {queue.ccb_buffer.begin() + prev_ccb_offset,
+               queue.ccb_buffer.begin() + queue.ccb_buffer_offset};
     }
 
-    return std::make_pair(dcb, ccb);
+    return {dcb, ccb};
 }
 
 void Liverpool::SubmitGfx(std::span<const u32> dcb, std::span<const u32> ccb) {
