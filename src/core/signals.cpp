ea38c1dba18dcdecde0b9a82283f7bcd26674b62
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-06-05 07:18:46 -0600

    mooooooreee

--- src/core/signals.cpp
+++ src/core/signals.cpp
@@ -79,28 +79,29 @@ static void SignalHandler(int sig, siginfo_t* info, void* raw_context) {
     case SIGBUS: {
         const bool is_write = Common::IsWriteError(raw_context);
         if (!signals->DispatchAccessViolation(raw_context, info->si_addr)) {
-        // UNREACHABLE_MSG(
-        //  "Unhandled access violation in thread '{}' at code address {}: {} address {}",
-        //  GetThreadName(), fmt::ptr(code_address), is_write ? "Write to" : "Read from",
-        //  fmt::ptr(info->si_addr));
+            // UNREACHABLE_MSG(
+            //  "Unhandled access violation in thread '{}' at code address {}: {} address {}",
+            //  GetThreadName(), fmt::ptr(code_address), is_write ? "Write to" : "Read from",
+            //  fmt::ptr(info->si_addr));
+        }
+        break;
+    }
+    case SIGILL:
+        if (!signals->DispatchIllegalInstruction(raw_context)) {
+            UNREACHABLE_MSG("Unhandled illegal instruction in thread '{}' at code address {}: {}",
+                            GetThreadName(), fmt::ptr(code_address),
+                            DisassembleInstruction(code_address));
+        }
+        break;
+    case SIGUSR1: { // Sleep thread until signal is received
+        sigset_t sigset;
+        sigemptyset(&sigset);
+        sigaddset(&sigset, SIGUSR1);
+        sigwait(&sigset, &sig);
     } break;
+    default:
+        break;
     }
-case SIGILL:
-    if (!signals->DispatchIllegalInstruction(raw_context)) {
-        UNREACHABLE_MSG("Unhandled illegal instruction in thread '{}' at code address {}: {}",
-                        GetThreadName(), fmt::ptr(code_address),
-                        DisassembleInstruction(code_address));
-    }
-    break;
-case SIGUSR1: { // Sleep thread until signal is received
-    sigset_t sigset;
-    sigemptyset(&sigset);
-    sigaddset(&sigset, SIGUSR1);
-    sigwait(&sigset, &sig);
-} break;
-default:
-    break;
-}
 }
 
 #endif
