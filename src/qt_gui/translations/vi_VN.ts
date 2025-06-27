0682d84bd040a9bb6ba3168697313692a45a0bdd
georgemoralis <giorgosmrls@gmail.com>
2025-06-27 09:36:47 +0300

    New Crowdin updates (#3152)
    * New translations en_us.ts (Turkish)
    
    * New translations en_us.ts (Portuguese, Brazilian)
    
    * New translations en_us.ts (Arabic)
    
    * New translations en_us.ts (Persian)
    
    * New translations en_us.ts (Catalan)
    
    * New translations en_us.ts (Serbian (Latin))
    
    * New translations en_us.ts (Swedish)
    
    * New translations en_us.ts (Romanian)
    
    * New translations en_us.ts (French)
    
    * New translations en_us.ts (Spanish)
    
    * New translations en_us.ts (Danish)
    
    * New translations en_us.ts (German)
    
    * New translations en_us.ts (Greek)
    
    * New translations en_us.ts (Finnish)
    
    * New translations en_us.ts (Hungarian)
    
    * New translations en_us.ts (Italian)
    
    * New translations en_us.ts (Japanese)
    
    * New translations en_us.ts (Korean)
    
    * New translations en_us.ts (Lithuanian)
    
    * New translations en_us.ts (Dutch)
    
    * New translations en_us.ts (Polish)
    
    * New translations en_us.ts (Portuguese)
    
    * New translations en_us.ts (Russian)
    
    * New translations en_us.ts (Slovenian)
    
    * New translations en_us.ts (Albanian)
    
    * New translations en_us.ts (Ukrainian)
    
    * New translations en_us.ts (Chinese Simplified)
    
    * New translations en_us.ts (Chinese Traditional)
    
    * New translations en_us.ts (Vietnamese)
    
    * New translations en_us.ts (Indonesian)
    
    * New translations en_us.ts (Norwegian Bokmal)
    
    * New translations en_us.ts (Vietnamese)
    
    * New translations en_us.ts (Norwegian Bokmal)
    
    * New translations en_us.ts (Swedish)
    
    * New translations en_us.ts (Catalan)
    
    * New translations en_us.ts (Russian)
    
    * New translations en_us.ts (Albanian)
    
    * New translations en_us.ts (Chinese Simplified)
    
    * New translations en_us.ts (Norwegian Bokmal)
    
    * New translations en_us.ts (Turkish)
    
    * New translations en_us.ts (Turkish)
    
    * New translations en_us.ts (Portuguese, Brazilian)

--- src/qt_gui/translations/vi_VN.ts
+++ src/qt_gui/translations/vi_VN.ts
@@ -748,6 +748,10 @@
       <source>Last updated</source>
       <translation>Cập nhật lần cuối</translation>
     </message>
+    <message>
+      <source>Favorite</source>
+      <translation type="unfinished">Favorite</translation>
+    </message>
   </context>
   <context>
     <name>GameListUtils</name>
@@ -928,7 +932,7 @@
     </message>
     <message>
       <source>No log file found for this game!</source>
-      <translation>!</translation>
+      <translation/>
     </message>
     <message>
       <source>Failed to convert icon.</source>
@@ -953,6 +957,14 @@
     <message>
       <source>SFO Viewer for </source>
       <translation type="unfinished">SFO Viewer for </translation>
+    </message>
+    <message>
+      <source>Remove from Favorites</source>
+      <translation type="unfinished">Remove from Favorites</translation>
+    </message>
+    <message>
+      <source>Add to Favorites</source>
+      <translation type="unfinished">Add to Favorites</translation>
     </message>
   </context>
   <context>
