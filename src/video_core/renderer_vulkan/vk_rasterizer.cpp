89c15723d57e84fa902f02dcc012d364a6f89c62
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-06-30 13:08:21 -0600

    clang

--- src/video_core/renderer_vulkan/vk_rasterizer.cpp
+++ src/video_core/renderer_vulkan/vk_rasterizer.cpp
@@ -343,7 +343,8 @@ void Rasterizer::DrawIndirect(bool is_indexed, VAddr arg_address, u32 offset, u3
     if (!pipeline) {
         return;
     }
-    if (auto* info = pipeline->GetStages()[u32(Shader::LogicalStage::Fragment)]; info && info->pgm_hash == 0xee1ee51b) {
+    if (auto* info = pipeline->GetStages()[u32(Shader::LogicalStage::Fragment)];
+        info && info->pgm_hash == 0xee1ee51b) {
         return;
     }
 
