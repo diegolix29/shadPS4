6092a8adc9ca75b3aaa48f2abed505d14697aec8
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-07-08 11:49:26 -0600

    clang

--- src/video_core/amdgpu/liverpool.cpp
+++ src/video_core/amdgpu/liverpool.cpp
@@ -236,11 +236,11 @@ Liverpool::Task Liverpool::ProcessGraphics(std::span<const u32> dcb, std::span<c
 
         switch (type) {
         default:
-           // UNREACHABLE_MSG("Wrong PM4 type {}", type);
+            // UNREACHABLE_MSG("Wrong PM4 type {}", type);
             break;
         case 0:
-            //UNREACHABLE_MSG("Unimplemented PM4 type 0, base reg: {}, size: {}",
-            //                header->type0.base.Value(), header->type0.NumWords());
+            // UNREACHABLE_MSG("Unimplemented PM4 type 0, base reg: {}, size: {}",
+            //                 header->type0.base.Value(), header->type0.NumWords());
             break;
         case 2:
             // Type-2 packet are used for padding purposes
