98d7bb2556126cdf880a0c88e61537e60c6fe020
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-07-08 10:22:10 -0600

    tomb unreadch

--- src/shader_recompiler/frontend/translate/translate.cpp
+++ src/shader_recompiler/frontend/translate/translate.cpp
@@ -290,13 +290,13 @@ T Translator::GetSrc(const InstOperand& operand) {
         break;
     case OperandField::Scc:
         if constexpr (is_float) {
-            UNREACHABLE();
+         //   UNREACHABLE();
         } else {
             value = ir.BitCast<IR::U32>(ir.GetScc());
         }
         break;
     default:
-        UNREACHABLE();
+        //UNREACHABLE();
     }
 
     if constexpr (is_float) {
