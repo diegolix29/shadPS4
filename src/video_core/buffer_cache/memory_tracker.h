f7a7f9f15e13511f6966c68feb2491147cf01ab1
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-07-26 12:31:44 -0600

    New fast readbacks wip (improved performance) independent from readbacks

--- src/video_core/buffer_cache/memory_tracker.h
+++ src/video_core/buffer_cache/memory_tracker.h
@@ -74,6 +74,9 @@ public:
                     if (Config::getReadbacksEnabled() &&
                         manager->template IsRegionModified<Type::GPU>(offset, size)) {
                         return true;
+                    } else if (Config::getFastReadbacksEnabled() &&
+                               manager->template IsRegionModified<Type::GPU>(offset, size)) {
+                        return true; // You can change this logic if needed
                     }
                     manager->template ChangeRegionState<Type::CPU, true>(
                         manager->GetCpuAddr() + offset, size);
--- src/video_core/buffer_cache/region_manager.h
+++ src/video_core/buffer_cache/region_manager.h
@@ -96,7 +96,9 @@ public:
         if constexpr (type == Type::CPU) {
             UpdateProtection<!enable, false>();
         } else if (Config::getReadbacksEnabled()) {
-            UpdateProtection<enable, true>();
+            UpdateProtection<enable, true>();        
+        } else if (Config::getFastReadbacksEnabled()) {
+            UpdateProtection<enable, false>();
         }
     }
 
@@ -128,6 +130,9 @@ public:
                 UpdateProtection<true, false>();
             } else if (Config::getReadbacksEnabled()) {
                 UpdateProtection<false, true>();
+            } else if (Config::getFastReadbacksEnabled()) {
+                UpdateProtection<false, true>(); // You can adjust this if fast readbacks require
+                                                 // different args
             }
         }
 
@@ -145,16 +150,21 @@ public:
     template <Type type>
     [[nodiscard]] bool IsRegionModified(u64 offset, u64 size) noexcept {
         RENDERER_TRACE;
-        const size_t start_page = SanitizeAddress(offset) / TRACKER_BYTES_PER_PAGE;
-        const size_t end_page =
-            Common::DivCeil(SanitizeAddress(offset + size), TRACKER_BYTES_PER_PAGE);
+        const u64 sanitized_offset = SanitizeAddress(offset);
+        const u64 sanitized_end = SanitizeAddress(offset + size);
+        const size_t start_page = sanitized_offset / TRACKER_BYTES_PER_PAGE;
+        const size_t end_page = Common::DivCeil(sanitized_end, TRACKER_BYTES_PER_PAGE);
         if (start_page >= NUM_PAGES_PER_REGION || end_page <= start_page) {
             return false;
         }
 
         const RegionBits& bits = GetRegionBits<type>();
-        RegionBits test(bits, start_page, end_page);
-        return test.Any();
+        for (size_t i = start_page; i < end_page; ++i) {
+            if (bits.Get(i)) [[unlikely]] {
+                return true;
+            }
+        }
+        return false;
     }
 
     LockType lock;
