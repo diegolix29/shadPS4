d2101143f6df243f031c414acbd6959e437003c6
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-08-05 17:57:32 -0600

    fixing sdl builds

--- src/core/ipc/ipc.cpp
+++ src/core/ipc/ipc.cpp
@@ -5,10 +5,12 @@
 
 #include <iostream>
 #include <string>
+#ifdef ENABLE_QT_GUI
 #include <QFile>
 #include <QJsonArray>
 #include <QJsonDocument>
 #include <QJsonObject>
+#endif
 
 #include "common/memory_patcher.h"
 #include "common/thread.h"
@@ -76,30 +78,40 @@ void IPC::Init() {
 }
 
 void IPC::InputLoop() {
-    auto next_str = [&] -> const std::string& {
-        static std::string line_buffer;
-        do {
-            std::getline(std::cin, line_buffer, '\n');
-        } while (!line_buffer.empty() && line_buffer.back() == '\\');
-        return line_buffer;
+    auto next_str = [&]() -> std::string {
+        std::string line, full_line;
+        while (true) {
+            if (!std::getline(std::cin, line))
+                break; // handle EOF
+            if (!line.empty() && line.back() == '\\') {
+                line.pop_back(); // remove trailing '\'
+                full_line += line;
+            } else {
+                full_line += line;
+                break;
+            }
+        }
+        return full_line;
     };
-    auto next_u64 = [&] -> u64 {
-        auto& str = next_str();
+
+    auto next_u64 = [&]() -> u64 {
+        auto str = next_str();
         return std::stoull(str, nullptr, 0);
     };
 
     while (true) {
-        auto& cmd = next_str();
+        std::string cmd = next_str();
         if (cmd.empty()) {
             continue;
         }
+
         if (cmd == "RUN") {
             run_semaphore.release();
         } else if (cmd == "START") {
             start_semaphore.release();
-        } else if (cmd == "PATCH_MEMORY") {
-        } else if (cmd == "CHEAT_ENABLE") {
-        } else if (cmd == "LOAD_CHEATS") {
+        }
+#ifdef ENABLE_QT_GUI
+        else if (cmd == "LOAD_CHEATS") {
             std::string activatedCheatsFile = m_cheatsDir + "/activated/cheats.json";
 
             QFile file(QString::fromStdString(activatedCheatsFile));
@@ -131,12 +143,17 @@ void IPC::InputLoop() {
                     applyCheatByName(cheatName);
                 }
             }
+        }
+#endif
+        else if (cmd == "PATCH_MEMORY") {
+        } else if (cmd == "CHEAT_ENABLE") {
         } else {
             std::cerr << "UNKNOWN CMD: " << cmd << std::endl;
         }
     }
 }
 
+#ifdef ENABLE_QT_GUI
 void IPC::applyCheatByName(const std::string& cheatName) {
     if (m_gameSerial.empty() || m_gameVersion.empty() || m_cheatsDir.empty()) {
         std::cerr << "IPC: Game info or cheats dir not set, cannot apply cheat\n";
@@ -183,7 +200,8 @@ void IPC::applyCheatByName(const std::string& cheatName) {
                 MemoryPatcher::PatchMemory(modNameStr, offsetStr, valueStr, targetStr, sizeStr,
                                            isOffset, littleEndian);
             }
-            break; // found cheat, no need to continue
+            break;
         }
     }
 }
+#endif
