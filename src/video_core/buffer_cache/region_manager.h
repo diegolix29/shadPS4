560e0c3d6dcd55ff3ddbd11c4a084505d9089fb3
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-08-09 08:38:33 -0600

    Readbacks opt added + fixes to merge and fast readbacks

--- src/video_core/buffer_cache/region_manager.h
+++ src/video_core/buffer_cache/region_manager.h
@@ -105,7 +105,7 @@ public:
                 ++flushes[page];
             }
         } else if (Config::getFastReadbacksEnabled()) {
-            UpdateProtection<enable, false>();
+            UpdateProtection<enable, true>();
 
             for (size_t page = start_page; page != end_page && !enable; ++page) {
                 ++flushes[page];
@@ -147,7 +147,7 @@ public:
                 if (readbacks) {
                     UpdateProtection<false, true>();
                 } else if (fast) {
-                    UpdateProtection<false, false>();
+                    UpdateProtection<true, false>();
                 }
 
                 if (!readbacks && !fast) {
