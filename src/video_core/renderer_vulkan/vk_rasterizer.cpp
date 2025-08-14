1a335daf1cc679949e588eb6a6137301fa90df2e
Dmugetsu <diegoavilafacebook75@gmail.com>
2025-08-14 17:30:17 -0600

    Smaller fix driveclub

--- src/video_core/renderer_vulkan/vk_rasterizer.cpp
+++ src/video_core/renderer_vulkan/vk_rasterizer.cpp
@@ -452,7 +452,6 @@ void Rasterizer::OnSubmit() {
         buffer_cache.ProcessFaultBuffer();
     }
     texture_cache.ProcessDownloadImages();
-
     texture_cache.RunGarbageCollector();
     buffer_cache.ProcessPreemptiveDownloads();
     buffer_cache.RunGarbageCollector();
