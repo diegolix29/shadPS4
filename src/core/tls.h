4ab92cf8efab7b5b1c7f3a81aff7c2631780b41c
Dmugetsu <diegoavilafacebook75@gmail.com>
2026-02-19 21:23:20 -0600

    Revert "GR2-win-crash-fix (#4033)"

--- src/core/tls.h
+++ src/core/tls.h
@@ -61,10 +61,7 @@ template <class ReturnType, class... FuncArgs, class... CallArgs>
 ReturnType ExecuteGuest(PS4_SYSV_ABI ReturnType (*func)(FuncArgs...), CallArgs&&... args) {
     EnsureThreadInitialized();
     // clear stack to avoid trash from EnsureThreadInitialized
-    auto* tcb = GetTcbBase();
-    if (tcb != nullptr && tcb->tcb_fiber == nullptr) {
-        ClearStack<12_KB>();
-    }
+    ClearStack<12_KB>();
     return func(std::forward<CallArgs>(args)...);
 }
 
