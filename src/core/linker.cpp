f466200ba0b875d6a2f7787b7fa744247beccc8e
Dmugetsu <diegoavilafacebook75@gmail.com>
2026-03-31 12:04:39 -0600

    Reverting Proper HLE implementation - breaks infamous on windows.

--- src/core/linker.cpp
+++ src/core/linker.cpp
@@ -15,7 +15,6 @@
 #include "core/devtools/widget/module_list.h"
 #include "core/libraries/kernel/memory.h"
 #include "core/libraries/kernel/threads.h"
-#include "core/libraries/sysmodule/sysmodule.h"
 #include "core/linker.h"
 #include "core/memory.h"
 #include "core/tls.h"
@@ -119,8 +118,7 @@ void Linker::Execute(const std::vector<std::string>& args) {
             ipc.WaitForStart();
         }
 
-        // Have libSceSysmodule preload our libraries.
-        Libraries::SysModule::sceSysmodulePreloadModuleForLibkernel();
+        LoadSharedLibraries();
 
         // Simulate libSceGnmDriver initialization, which maps a chunk of direct memory.
         // Some games fail without accurately emulating this behavior.
@@ -359,10 +357,8 @@ bool Linker::Resolve(const std::string& name, Loader::SymbolType sym_type, Modul
         return_info->virtual_address = AeroLib::GetStub(sr.name.c_str());
         return_info->name = "Unknown !!!";
     }
-    if (library->name != "libc" && library->name != "libSceFios2") {
-        LOG_WARNING(Core_Linker, "Linker: Stub resolved {} as {} (lib: {}, mod: {})", sr.name,
-                    return_info->name, library->name, module->name);
-    }
+    LOG_ERROR(Core_Linker, "Linker: Stub resolved {} as {} (lib: {}, mod: {})", sr.name,
+              return_info->name, library->name, module->name);
     return false;
 }
 
