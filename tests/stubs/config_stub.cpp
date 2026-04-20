aedaaa1145ea2715d76533d129fb1db05173b6fb
Dmugetsu <diegoavilafacebook75@gmail.com>
2026-04-20 07:17:24 -0600

    Spdlog off

---
+++ tests/stubs/config_stub.cpp
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: Copyright 2026 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "common/config.h"
+
+namespace Config {
+
+bool directMemoryAccess() {
+    return false;
+}
+
+bool dumpShaders() {
+    return false;
+}
+
+} // namespace Config
