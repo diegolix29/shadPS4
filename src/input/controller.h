0e28806aaf6e49793615b1fd5f74121dcbb4057f
Dmugetsu <diegoavilafacebook75@gmail.com>
2026-05-07 11:30:39 -0600

    Revert "Avoid out-of-bounds array access when checking custom color for TV Remote (#4356)"
    This reverts commit 4d3827c34949d034cc47e86c943b7fd9318c48ae.

--- src/input/controller.h
+++ src/input/controller.h
@@ -201,9 +201,6 @@ public:
         controller_override_colors[i] = {r, g, b};
     }
     static std::optional<Colour> GetControllerCustomColor(s32 i) {
-        if (i >= controller_override_colors.size()) {
-            return {};
-        }
         return controller_override_colors[i];
     }
 };
