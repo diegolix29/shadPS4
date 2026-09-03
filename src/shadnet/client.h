2c59ba4ab530ef7ae4f60b1ea242652556e150d3
Diego Mugetsu <diegoavilafacebook75@gmail.com>
2026-09-03 09:41:43 -0600

    Revert "Tss support (#4930)"
    This reverts commit d7114f91971e1b1ba0c2476f5a6fca68c01b2b85.

--- src/shadnet/client.h
+++ src/shadnet/client.h
@@ -110,7 +110,6 @@ enum class CommandType : u16 {
     TusTryAndSetVariable = 211,
     TusGetFriendsVariable = 212,
     TusDeleteMultiSlotVariable = 213,
-    TssGetData = 214,
     // Trophies
     UnlockTrophy = 301,
     SyncTrophies = 302,
@@ -161,7 +160,6 @@ enum class ErrorType : uint8_t {
     ScoreHasData = 31,
     CondFail = 32,
     Unsupported = 33,
-    TooLarge = 34, // TSS file on the server exceeds the slot's documented maximum
 };
 
 enum class ShadNetState {
