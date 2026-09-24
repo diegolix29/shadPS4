90f232ed6ab3e1115b9b243bef6620cf771d1c14
Diego Mugetsu <diegoavilafacebook75@gmail.com>
2026-09-24 10:25:22 -0600

    revert order hack

--- src/shader_recompiler/backend/spirv/emit_spirv_image.cpp
+++ src/shader_recompiler/backend/spirv/emit_spirv_image.cpp
@@ -5,7 +5,6 @@
 #include "common/memory_patcher.h"
 #include "shader_recompiler/backend/spirv/emit_spirv_instructions.h"
 #include "shader_recompiler/backend/spirv/spirv_emit_context.h"
-#include "shader_recompiler/ir/microinstruction.h"
 
 namespace Shader::Backend::SPIRV {
 
@@ -51,7 +50,7 @@ struct ImageOperands {
             Add(spv::ImageOperandsMask::ConstOffset, ctx.ConstS32(operand));
             return;
         }
-        IR::Inst* const inst{offset.Inst()};
+        IR::Inst* const inst{offset.InstRecursive()};
         if (inst->AreAllArgsImmediates()) {
             switch (inst->GetOpcode()) {
             case IR::Opcode::CompositeConstructU32x2:
@@ -252,22 +251,15 @@ Id EmitImageRead(EmitContext& ctx, IR::Inst* inst, u32 handle, Id coords, Id lod
     const auto& texture = ctx.images[handle & 0xFFFF];
     const Id color_type = texture.data_types->Get(4);
     ImageOperands operands;
+    operands.Add(spv::ImageOperandsMask::Sample, ms);
     Id texel;
     if (!texture.is_storage) {
         const Id image = ctx.OpLoad(texture.image_type, texture.id);
-        if (texture.view_type == AmdGpu::ImageType::Color2DMsaa) {
-            // GCN hardware wraps out-of-range MSAA sample indices
-            if (Sirit::ValidId(ms)) {
-                const Id sample_count = ctx.OpImageQuerySamples(ctx.U32[1], image);
-                const Id wrapped_ms = ctx.OpUMod(ctx.U32[1], ms, sample_count);
-                operands.Add(spv::ImageOperandsMask::Sample, wrapped_ms);
-            }
-        } else {
+        if (texture.view_type != AmdGpu::ImageType::Color2DMsaa) {
             if (Sirit::ValidId(ms)) {
                 LOG_ERROR(Render_Recompiler, "image is not MS but ms operand is provided");
             }
             operands.Add(spv::ImageOperandsMask::Lod, lod);
-            operands.Add(spv::ImageOperandsMask::Sample, ms);
         }
         texel = ctx.OpImageFetch(color_type, image, coords, operands.mask, operands.operands);
     } else {
@@ -276,7 +268,7 @@ Id EmitImageRead(EmitContext& ctx, IR::Inst* inst, u32 handle, Id coords, Id lod
             operands.Add(spv::ImageOperandsMask::Lod, lod);
         } else if (Sirit::ValidId(lod)) {
 #if 1
-            // It's confusing what interactions will cause this code path so leave it as
+            // It's  confusing what interactions will cause this code path so leave it as
             // unreachable until a case is found.
             // Normally IMAGE_LOAD_MIP should translate -> OpImageFetch
             UNREACHABLE_MSG("Unsupported ImageRead with Lod");
